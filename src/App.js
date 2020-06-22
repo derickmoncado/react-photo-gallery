12af59d5116103a03542e684bbc9d4a2aad6eb2a
Derick Moncado <derickmoncado@gmail.com>
2020-06-21 21:16:15 -0700

    Added component and trying to set up API call with hooks.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import PhotoList from "./components/PhotoList";
 
 function App() {
+	const [images, setItems] = useState([]);
+
+	useEffect(() => {
+			fetch('https://picsum.photos/v2/list')
+			.then(res => res.json())
+			.then(data => this.setState({
+				images: data
+			})
+	}, [])
+
 	return (
 		<div className="App">
-			<p>my photo gallery app (WIP)</p>
-			<div className="data">
-				<ul>
-					<li>asdf</li>
-				</ul>
-			</div>
+			<h1>Random Photo Gallery</h1>
+			<PhotoList />
 		</div>
 	);
 }
---
+++ src/components/PhotoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PhotoList() {
+
+	return (
+		<div className="photo-container">
+			<ul>
+				<li>asdf</li>
+				<li>asdfasdf</li>
+				<li>asdfasdf</li>
+			</ul>
+		</div>
+	);
+}
+
+export default PhotoList;
