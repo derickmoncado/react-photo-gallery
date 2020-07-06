d298bba8cdd73ab14d6a5299131f3a70c93ec4c3
Derick Moncado <derickmoncado@gmail.com>
2020-07-06 14:01:02 -0700

    Prettified.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import "./App.scss";
 import PhotoList from "./components/PhotoList";
 
 function App() {
-	const [images, setImages] = useState([]);
+  const [images, setImages] = useState([]);
 
-	useEffect(() => {
-		fetch('https://picsum.photos/v2/list?page=2&limit=16')
-		.then(res => res.json())
-		.then(data => {
-			console.log(data)
-			setImages(data)
-		})
-	}, [])
+  useEffect(() => {
+    fetch("https://picsum.photos/v2/list?page=2&limit=16")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setImages(data);
+      });
+  }, []);
 
-	return (
-		<div className="App">
-			<h1>Random Photo Gallery App</h1>
-			<PhotoList images={images} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>Random Photo Gallery App</h1>
+      <PhotoList images={images} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,36 +1,34 @@
 import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 function PhotoList({ images }) {
-	const [show, setShow] = useState(false);
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
-	//const thisImageID = images.filter();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  //const thisImageID = images.filter();
 
-	return (
-		<div className="photo-container">
-			<ul>
-				{images.map(image => (
-					<li key={image.id}>
-						<Button onClick={handleShow}>
-							<img src={image.download_url} alt="Random pic" />
-							<p>Author: {image.author}</p>
-						</Button>
-					</li>
-				))}
-			</ul>
+  return (
+    <div className="photo-container">
+      <ul>
+        {images.map(image => (
+          <li key={image.id}>
+            <Button onClick={handleShow}>
+              <img src={image.download_url} alt="Random pic" />
+              <p>Author: {image.author}</p>
+            </Button>
+          </li>
+        ))}
+      </ul>
 
-			<Modal show={show} onHide={handleClose} centered>
-				<Modal.Header closeButton>
-					<Modal.Title>Photo By: [author]</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					[photo]
-				</Modal.Body>
-			</Modal>
-		</div>
-	);
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Photo By: [author]</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>[photo]</Modal.Body>
+      </Modal>
+    </div>
+  );
 }
 
 export default PhotoList;
