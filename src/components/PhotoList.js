8e551457c03332f0d99d9b14b12e3ec2a345a300
Derick Moncado <derickmoncado@gmail.com>
2021-03-10 18:57:48 -0800

    Bro idk

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -6,8 +6,7 @@ const PhotoList = ({ images }) => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = (imageID) => {
-    setShow(true);
-    console.log(imageID);
+    setShow(imageID);
   };
 
   return (
@@ -15,7 +14,7 @@ const PhotoList = ({ images }) => {
       <ul>
         {images.map(image => (
           <li key={image.id}>
-            <Button onClick={() => {handleShow(image.id)}}>
+            <Button onClick={() => handleShow(image.id)}>
               <img src={image.download_url} alt="Random pic" />
               <p>Author: {image.author}</p>
             </Button>
@@ -25,7 +24,7 @@ const PhotoList = ({ images }) => {
 
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>Photo By: author name here</Modal.Title>
+          <Modal.Title>Photo By: author name</Modal.Title>
         </Modal.Header>
         <Modal.Body>photo goes here</Modal.Body>
       </Modal>
